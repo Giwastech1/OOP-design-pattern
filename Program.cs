4184ac140921983de09f1e09d21065ab7f62b104
giwastech1 <giwatoheeb27@gmail.com>
2026-08-17 23:43:35 +0100

    refactor to obey encapsulation

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿//creating bad bad account balnce.
+using System.Reflection.Metadata;
+
 BadBankAccount badAccount = new BadBankAccount();
 badAccount.balance = 5968;
 Console.WriteLine(badAccount.balance);
 Console.WriteLine();
 
 //creating an account with encapsulation principle.
+/*
 BankAccount account = new();
 decimal balance =account.Deposit(3400);
 Console.WriteLine(balance);
 
 BankAccount account2 = new();
 Console.WriteLine(account2.Deposit(100));
+*/
+
+BankAccount user = new BankAccount();
+user.Deposit(400);
+Console.WriteLine(user.Balance());
+user.Deposit(350.25m);
+user.Withdraw(200);
+Console.WriteLine(user.Balance());
--- src/OopPrinciples/BankAccount.cs
+++ src/OopPrinciples/BankAccount.cs
@@ -3,8 +3,25 @@ class BankAccount
 {
     private decimal balance;
 
+    /*
     public decimal Deposit(decimal amount)
     {
        return balance += amount;
+    }
+    */
+
+    public decimal Deposit(decimal amount)
+    {
+        return this.balance = this.balance + amount;
+    }
+
+    public decimal Withdraw(decimal amount)
+    {
+        return this.balance = this.balance - amount;
+    }
+
+    public decimal Balance()
+    {
+        return this.balance;
     }
 }
